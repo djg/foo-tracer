b9d7f9fbbd89c6b7489e819a905154247193d9cb
Dan Glastonbury <dan.glastonbury@gmail.com>
2018-09-09 17:47:24 +1000

    Better naming of Camera state.

--- src/camera.rs
+++ src/camera.rs
@@ -9,21 +9,20 @@ fn random_in_unit_disk() -> Vec3 {
 }
 
 pub struct Camera {
-    pub origin: Vec3,
-    pub lower_left_corner: Vec3,
-    pub horizontal: Vec3,
-    pub vertical: Vec3,
-    pub u: Vec3,
-    pub v: Vec3,
-    pub w: Vec3,
-    pub lens_radius: f32,
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+    right: Vec3,
+    up: Vec3,
+    lens_radius: f32,
 }
 
 impl Camera {
     pub fn new(
         look_from: Vec3,
         look_at: Vec3,
-        vup: Vec3,
+        up: Vec3,
         vfov: f32,
         aspect: f32,
         aperture: f32,
@@ -34,26 +33,26 @@ impl Camera {
         let half_height = f32::tan(theta / 2.);
         let half_width = aspect * half_height;
         let origin = look_from;
-        let w = normalized(look_from - look_at);
-        let u = normalized(cross(vup, w));
-        let v = cross(w, u);
-        let lower_left_corner = origin - focus_dist * (half_width * u + half_height * v + w);
-        let horizontal = 2. * half_width * focus_dist * u;
-        let vertical = 2. * half_height * focus_dist * v;
+        let back = normalized(look_from - look_at);
+        let right = normalized(cross(up, back));
+        let up = cross(back, right);
+        let lower_left_corner =
+            origin - focus_dist * (half_width * right + half_height * up + back);
+        let horizontal = 2. * half_width * focus_dist * right;
+        let vertical = 2. * half_height * focus_dist * up;
         Camera {
             origin,
             lower_left_corner,
             horizontal,
             vertical,
-            u,
-            v,
-            w,
+            right,
+            up,
             lens_radius,
         }
     }
     pub fn ray(&self, u: f32, v: f32) -> Ray {
         let rd = self.lens_radius * random_in_unit_disk();
-        let offset = self.u * rd.0 + self.v * rd.1;
+        let offset = self.right * rd.0 + self.up * rd.1;
         Ray {
             point: self.origin + offset,
             direction: self.lower_left_corner + u * self.horizontal + v * self.vertical
